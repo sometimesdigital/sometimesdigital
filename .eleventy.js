459fbd5faf0800f05cbe395c90c9cc4185ba19a8
tusindfryd <20538005+tusindfryd@users.noreply.github.com>
2023-02-12 17:29:48 +0100

    init

---
+++ .eleventy.js
@@ -0,0 +1,32 @@
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const pluginDate = require("eleventy-plugin-date");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(syntaxHighlight);
+  eleventyConfig.addPlugin(pluginDate);
+  eleventyConfig.addPlugin(pluginRss);
+  eleventyConfig.addFilter("smartquotes", (post) => {
+    const hawaii = new RegExp(/(?<=<(h|l|p[^r]).*)Hawai'i/g);
+    const slang = new RegExp(/'(cause|em|til|twas)/g);
+    const apostrophes = new RegExp(/(?<=<(h|l|p[^r]).*)\b'\b/g);
+    const years = new RegExp(/(?<=\s)'(?=\d)/g);
+    const openDoubles = new RegExp(/(?<=<(h|l|p[^r]).*)(?<=\s|>)&quot;/g);
+    const closeDoubles = new RegExp(
+      /(?<=<(h|l|p[^r]).*“.*)&quot;(?=(\s|\p{P}|<))/gu
+    );
+    const openSingles = new RegExp(/(?<=<(h|l|p[^r]).*)(?<=\s|>)'/g);
+    const closeSingles = new RegExp(
+      /(?<=<(h|l|p[^r]).*‘.*)'(?=(\s|\p{P}|<))/gu
+    );
+    return post
+      .replace(hawaii, "Hawaiʻi")
+      .replace(slang, "’$1")
+      .replace(apostrophes, "’")
+      .replace(years, "’")
+      .replace(openDoubles, "“")
+      .replace(closeDoubles, "”")
+      .replace(openSingles, "‘")
+      .replace(closeSingles, "’");
+  });
+};
